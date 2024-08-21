d8d6091658f15b9b245ebf1e47fae566630d0ab1
ǝǝq-⅄uuoS <70021784+Velkostatkar@users.noreply.github.com>
2024-08-21 18:00:07 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+--SonnyBee QBCore TODO
+--Convert to new CreateThreads
+--Kill all Citizens
+--Convert to QBCore.Functions 
+--Check Resmon
+--Check Profiler
+--Be sure that DRIFT is feelin GOOD
+
+
+fx_version 'bodacious'
+game 'gta5'
+
+author 'SonnyBee QB-CORE'
+description 'Shift, drift mode ON/OFF'
+version '0.27.0'
+
+client_script 'cl.lua'
